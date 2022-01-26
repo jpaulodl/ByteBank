74be2520e54dc6db961192644866823f186afb53
João Paulo Dolcimásculo <95173709+jpaulodl@users.noreply.github.com>
2022-01-26 18:27:01 -0300

    Add files via upload

---
+++ conta.py
@@ -0,0 +1,55 @@
+# __ deixa o atributo privado
+# get - GETTERS devolve / @property
+# set - SETTERS recebe / @nome.setter
+# Metodo da class / @staticmethod Chama metodo sem acessa a class!
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Contruindo Objeto ...{self}")
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        self.__codigo_banco = "001"
+
+    def extrato(self):
+        print(f'Saldo R$ {self.__saldo} do titular {self.__titular}.')
+    
+    def depositar(self, valor):
+        self.__saldo += valor 
+       
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
+    def sacar(self, valor):
+        limite_de_saque = self.__saldo + self.__limite
+        if (self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else: 
+            print(f"Passou o valor disponível para saque, o valor é de R$ {limite_de_saque}")
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+    
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa':'104', 'Bradesco':'237'}
